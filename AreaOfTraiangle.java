2ecf12a957f85892a00f05e29aff60a145836760
Mohanraj0013 <114380798+Mohanraj0013@users.noreply.github.com>
2022-10-28 11:38:13 +0530

    Add files via upload

---
+++ AreaOfTraiangle.java
@@ -0,0 +1,13 @@
+package firstproject;
+import java.util.Scanner;
+public class AreaOfTraiangle {
+	public static void main(String args[]) {       
+		Scanner scanner = new Scanner(System.in); 
+	 
+    System.out.println("Enter the width of the Triangle:");     
+  double base = scanner.nextDouble(); 
+
+    System.out.println("Enter the height of the Triangle:");        
+    double height = scanner.nextDouble(); 
+}
+}
---
+++ CastingExercise.java
@@ -0,0 +1,16 @@
+package firstproject;
+
+public class CastingExercise {
+	public static void main(String[] args) {
+		byte b=10;
+
+		int i=b;
+	    int c=i;
+		byte d=(byte)i;
+		byte x=10;
+		byte y=20;
+	    int sum= x*y;
+	    System.out.println(sum);
+		}
+	}
+
